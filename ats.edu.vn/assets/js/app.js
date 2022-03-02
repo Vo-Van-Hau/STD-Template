26cf9426c447fe63cada15405904334f79337d69
pthieenlong <p.thieenlong.304@gmail.com>
2022-03-02 16:31:14 +0700

    slide animation

--- ats.edu.vn/assets/js/app.js
+++ ats.edu.vn/assets/js/app.js
@@ -1,3 +1,26 @@
-const cateItem = $('.category-item');
-
-console.log(cateItem);
+function showCategoryMenu(parent) {
+    $(parent).children('ul').slideToggle();
+}
+// headSearch
+let isHidden = false;
+$('#headSearchBtn').click(() => {
+    if(isHidden === false) {
+        $('.menubar__list-item > a').css({
+            animation: 'slideLeft 1s forwards',
+        })
+        $('#headSearch').css({
+            animation: 'searchSlideLeft 1s forwards',
+            // display: 'inline',
+        });
+        isHidden = true;
+    } else {
+        $('.menubar__list-item > a').css({
+            animation: 'slideRight 1s forwards',
+        })
+        $('#headSearch').css({
+            animation: 'searchSlideRight 1s forwards',
+            // display: 'none',
+        });
+        isHidden = false;
+    }
+});
